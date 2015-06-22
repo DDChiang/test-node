4640359ec178ff510b213f9cb74bd734d0190f63
DDChiang <ddeechng@gmail.com>
2015-06-21 22:57:15 -0700

    set viewport maxwidth for grocery app

---
+++ sites/dodge/js/app.js
@@ -0,0 +1,8 @@
+var dodger = angular.module('dodge', []);
+dodger.controller('dodgeCtrl', function ($scope) {
+	$scope.message = "hello dodge";
+});
+
+
+//strucutre
+//boxes(widgets) = pic + title     +    content(title + descr) 
